db45444d158faf55d592fd514a7ad9bf9c3c21af
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-07-02 11:40:43 +0800

    y

--- src/plugin/axios2.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = `https://api.apico.dev/v1/zPVUac/`
-axios.defaults.headers.get['Accept'] = 'application/json';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-
-export default axios;
