13ef6d38e1dcdd213e82eecc12ed8c2ca175f553
iannico322 <iannicocaulin@gmail.com>
2025-08-15 10:07:30 +0800

    Updates

---
+++ assets/axios-322329e2.js
@@ -0,0 +1,1 @@
+import{a}from"./axios-e136f1a1.js";a.defaults.baseURL="";a.defaults.headers.get.Accept="application/json";a.defaults.headers.post["Content-Type"]="application/json";
--- assets/axios-caa86010.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./axios-44d4ce88.js";a.defaults.baseURL="";a.defaults.headers.get.Accept="application/json";a.defaults.headers.post["Content-Type"]="application/json";
