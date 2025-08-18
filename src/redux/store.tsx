a7ca3e9c12b3085f76859a757d6029cfd460c1e5
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-08-18 15:57:01 +0800

    y

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -17,6 +17,7 @@ import reportFilterReducer from './reportFilterSlice';
 import businessPermitTableReducer from './businessPermitSlice';
 import workingPermitTableReducer from './workingPermitTableSlice';
 import brgyClearanceTableReducer from './brgyClearanceTableSlice'; 
+import WpReducer from './wpSlice';
 import BrgyReducer from './brgySlice';
 import buildingPermitReducer from './buildingPermitSlice';
 import certificateOfOccupancyReducer from './CertificateOfOccupancySlice';
@@ -58,6 +59,7 @@ const rootReducer = combineReducers({
   separatedTransaction: separatedTransactionReducer,
   load: loadReducer,
   status: statusReducer,
+  wp:WpReducer,
   reportFilter: reportFilterReducer,
   businessPermitTable: businessPermitTableReducer,
   workingPermitTable: workingPermitTableReducer,
